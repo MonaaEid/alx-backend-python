5a1df15e0c4445c5415e0df2e0549d01d69067a1
MonaaEid <monaa_eid@hotmail.com>
2024-04-18 23:04:58 +0200

    update

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """function make_multiplier that takes a float multiplier
 as argument and returns a function that multiplies a float by multiplier"""
+from typing import Callable
 
 
-def make_multiplier(multiplier: float) -> callable:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """returns a function that multiplies a float by multiplier"""
     return lambda n: n * multiplier
