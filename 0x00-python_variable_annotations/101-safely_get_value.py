78d3a899c129ad4de598de5c5ec9e3a6d4a62ad7
MonaaEid <monaa_eid@hotmail.com>
2024-04-18 22:36:47 +0200

    update

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,8 +1,16 @@
-#usr/bin/env python3
+#!/usr/bin/env python3
 """type-annotated function safely_get_value that takes a dict and a key as
 arguments and returns the value of the key"""
 from typing import Union, Any, Dict
 
+
+def safely_get_value(dct: Dict, key: Any, default: Union[Any, None] = None) -> Union[Any, None]:
+    """returns the value of the key"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
+    
 
 def safely_get_value(dct, key, default = None):
     if key in dct:
---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""type-annotated function type_checking that takes a variable x, and returns the
+sum as a float"""
+from typing import Union, Tuple, List
+
+
+def type_checking(x: Union[int, float], y: Union[int, float]) -> float:
+    """returns the sum of x and y as a float"""
+    return x + y
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+    """returns a zoomed in list"""
+    zoomed_in: Tuple = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3.0)
