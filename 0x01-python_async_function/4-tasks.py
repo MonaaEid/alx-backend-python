372bc4a8dc1cbbf57857ff7f3fad929ef84a2932
MonaaEid <monaa_eid@hotmail.com>
2024-04-23 02:27:21 +0200

    Asynchronous coroutine

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -3,7 +3,9 @@
 
 import asyncio
 from typing import List
-task_wait_random = __import__('3-tasks').task_wait_random
+wait_random = __import__(
+    '0-basic_async_syntax').wait_randomwait_random = __import__(
+        '0-basic_async_syntax').wait_random
 
 
 def task_wait_n(n: int, max_delay: int) -> List[float]:
@@ -18,5 +20,5 @@ def task_wait_n(n: int, max_delay: int) -> List[float]:
     Returns:
         List[float]: List of delays in increasing order.
     """
-    delays = [task_wait_random(max_delay) for _ in range(n)]
+    delays = [wait_random(max_delay) for _ in range(n)]
     return [await delay for delay in asyncio.as_completed(delays)]
