6d2c5916dd2cb9e1c76cd84e432913dbd2dfdead
MonaaEid <monaa_eid@hotmail.com>
2024-04-23 02:16:59 +0200

    Asynchronous coroutine

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,7 +6,8 @@ import random
 from typing import List
 
 wait_random = __import__(
-    '0-basic_async_syntax').wait_randomwait_random = __import__('0-basic_async_syntax').wait_random
+    '0-basic_async_syntax').wait_randomwait_random = __import__(
+        '0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""Measure the runtime"""
+import asyncio
+import time
+from typing import List
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def runtime(n: int, max_delay: int) -> float:
+    """
+    Function that measures the total runtime and returns it.
+    """
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.time()
+    return (end - start) / n
