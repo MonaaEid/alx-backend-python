db449350489b919c6420094cfdbd4da7a76edbf3
MonaaEid <monaa_eid@hotmail.com>
2024-05-14 04:29:16 +0300

    Add the tasks files

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -23,6 +23,11 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test access_nested_map function"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+
     def test_access_nested_map_exception(self,
             nested_map: Dict,
             path: Tuple[str],
