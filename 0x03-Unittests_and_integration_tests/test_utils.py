ef61dff929f5f51e2a8d9ecb61c7b09d30675d4e
MonaaEid <monaa_eid@hotmail.com>
2024-05-14 04:28:18 +0300

    Add the tasks files

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,25 +9,28 @@ from utils import access_nested_map, get_json, memoize
 
 class TestAccessNestedMap(unittest.TestCase):
     """Test access_nested_map function"""
-
     @parameterized.expand([
         ({'a': 1}, ('a',), 1),
         ({'a': {'b': 2}}, ('a',), {'b': 2}),
         ({'a': {'b': 2}}, ('a', 'b'), 2)
     ])
     def test_access_nested_map(
-            self, nested_map: Dict, path: Tuple, expected: Union[Dict, int]):
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            expected: Union[Dict, int],
+            ) -> None:
         """Test access_nested_map function"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
-    @parameterized.expand([
-        ({}, ("a",), KeyError),
-        ({"a": 1}, ("a", "b"), KeyError),
-    ])
-    def test_access_nested_map_exception(self):
+    def test_access_nested_map_exception(self,
+            nested_map: Dict,
+            path: Tuple[str],
+            exception: Exception,
+            ) -> None:
         """Test access_nested_map function with exception"""
-        with self.assertRaises(KeyError):
-            access_nested_map({'a': 1}, ('a', 'b'))
+        with self.assertRaises(exception):
+            access_nested_map(nested_map, path)
 
 
 class TestGetJson(unittest.TestCase):
